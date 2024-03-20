361926cd065c1a6f655168b0b0dd6d852289e354
subhankari2 <padhisubhankari@gmail.com>
2024-03-20 18:52:47 +0530

    jsx expressions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import Heading from "./Heading"
-function App() {
-  return (<div>
-   <Heading firstName="Bob"/>
-   <Heading firstName="julie"/>
-   <Heading firstName="karl"/>
-   </div>
+const bool = false;
+const str1 = "just";
+
+function Example(props) {
+    return (
+        <div>
+            <h2>
+                The value of the toggleBoolean prop is:{props.toggleBoolean.toString()}
+            </h2>
+            <p>The value of the math prop is: <em>{props.math}</em></p>
+            <p>The value of the str prop is: <em>{props.str}</em></p>
+        </div>
     );
-}
+};
+
+function App() {
+    return (
+        <div className="App">
+            <Example
+                toggleBoolean={!bool}
+                math={(10 + 20) / 3}
+                str={str1 + ' another ' + 'string'}
+            />
+        </div>
+    );
+};
+
+// function App() {
+//   return (<div>
+
+//    <Heading firstName="Bob"/>
+//    <Heading firstName="julie"/>
+//    <Heading firstName="karl"/>
+//    </div>
+//     );
+// }
 
 export default App;
